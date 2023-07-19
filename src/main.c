4549ddc5701bad9584a70a415e85097168279d0a
8dcc <8dcc.git@gmail.com>
2023-07-19 20:24:34 +0200

    Add constructor comment

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/* #include <dlfcn.h> */
 
+/*
+ * We need:
+ *   __attribute__((constructor))
+ * To indicate that this function will be the entry point once injected.
+ */
 __attribute__((constructor)) void load(void) {
     printf("hl-cheat loaded!\n");
 }
