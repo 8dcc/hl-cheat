603baef039fb53a6d7339835a7bead65489f8cfd
8dcc <8dcc.git@gmail.com>
2023-07-19 20:13:24 +0200

    Add injector script

---
+++ inject.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pid=$(pidof "hl_linux")
+libpath=$(realpath "libhlcheat.so")
+
+# 0x2 -> RTLD_NOW
+sudo gdb -n -q -batch                                   \
+    -ex "attach $pid"                                   \
+    -ex "set \$dlopen = (void* (*)(char*, int))dlopen"  \
+    -ex "set \$dlerror =  (char* (*)(void))dlerror"     \
+    -ex "call \$dlopen(\"$libpath\", 2)"                \
+    -ex "call \$dlerror()"                              \
+    -ex "detach"                                        \
+    -ex "quit"
+
+#   -ex "set \$dlclose = (int (*)(void*))dlclose"       \
+
+if [ $? -eq 0 ]; then
+    echo -e "\nhl-cheat loaded!"
+fi
