e3af8bd39acf4508daf50e83567ed4aba1055777
8dcc <8dcc.git@gmail.com>
2023-07-20 23:21:01 +0200

    Call bhop after original in CL_CreateMove

--- src/hooks.c
+++ src/hooks.c
@@ -3,6 +3,8 @@
 #include "include/sdk.h"
 #include "include/globals.h"
 
+#include "features/include/movement.h"
+
 DECL_HOOK(CL_CreateMove);
 
 bool hooks_init(void) {
@@ -12,7 +14,7 @@ bool hooks_init(void) {
 }
 
 void h_CL_CreateMove(float frametime, usercmd_t* cmd, int active) {
-    /* bhop(cmd); */
-
     ORIGINAL(CL_CreateMove, frametime, cmd, active);
+
+    bhop(cmd);
 }
