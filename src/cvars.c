24c1d796d90136f3683b1c10ffaa8a08dc05f65e
8dcc <8dcc.git@gmail.com>
2023-07-21 15:49:01 +0200

    Add esp cvar

--- src/cvars.c
+++ src/cvars.c
@@ -4,9 +4,11 @@
 #include "include/globals.h"
 
 DECL_CVAR(bhop);
+DECL_CVAR(esp);
 
 bool cvars_init(void) {
     cv_bhop = REGISTER_CVAR("bhop", "1");
+    cv_esp  = REGISTER_CVAR("esp", "1");
 
     return true;
 }
